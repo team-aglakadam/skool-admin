dbe60688447ce9ffb40b974667fe66d2547c9eb3
HaRish46 <43755251+HaRish46@users.noreply.github.com>
2025-08-03 12:08:55 +0530

    Create user.ts

---
+++ types/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: string
+  school_id: string
+  email: string
+  role: string
+  full_name: string
+  phone: string
+  created_at: string
+  address: string
+  date_of_birth: string
+  blood_group: string
+}
