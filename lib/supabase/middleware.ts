090100b6fdb592b7fb2f354c83e76d09d9873c15
Krishnama  Naidu Senapathi <31931582+coding-yoddha@users.noreply.github.com>
2026-06-29 12:38:40 +0530

    Update middleware to allow access to dashboard without auth

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -38,8 +38,7 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/') &&
-    request.nextUrl.pathname !== '/'
+    request.nextUrl.pathname.startsWith('/dashboard')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
